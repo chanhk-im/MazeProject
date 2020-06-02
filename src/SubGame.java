a0f474ca38af905b42a8b6f75ab9b62b4bd88ddd
Handong-Promi <21900543@handong.edu>
2020-06-02 12:57:24 +0900

    add Subgame.java for providing sub-game interface

---
+++ src/SubGame.java
@@ -0,0 +1,33 @@
+
+public class SubGame{
+
+	// through this mehtod, a player can play one of the 3 sub games; rsp, up-down, and quiz. If player win, this method return true. If not, returns false. This return statement can help the Maze to make the player characteristics (vision, life, moving range, etc - there are not-implemented characteristic)
+	public boolean playSubgame(){
+	    int choose = chooseGameRandomly();
+		switch(choose){
+			case 1:
+				return // Rock-Scissor-Paper game play
+			case 2:
+				return // Up donw game play
+			case 3:
+				return Quiz.playQuiz();
+		}
+	}
+
+	public boolean isSubgame(Player explorer, int[][] maze){
+
+	    int pX = explorer.getX();
+	    int pY = explorer.getY();
+
+	    // 2 in maze txt file means "there is a sub game". it should be connected other 'opened' path (opened path means 1).
+	    if(maze[pX][pY] == 2){
+	        return true;
+	    }
+	    return false;
+	}
+
+	private int chooseGameRandomly(){
+		Random generator = new Random();
+		int index = generator.nextInt(3) + 1; // because the number of sub games is 3.
+		return index;
+}
